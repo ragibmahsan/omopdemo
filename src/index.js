a7183820197b8f163a938d6826b9826212f16740
Ragib Ahsan <ragibmahsan@gmail.com>
2025-05-13 12:20:04 -0400

    pool

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/root';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
+import { Amplify } from 'aws-amplify';
+import amplifyConfig from './amplifyconfiguration';
+
+// Configure Amplify
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
