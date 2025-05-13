33305bf7fda578f92a4752f5128bab2ff227e8eb
Ragib Ahsan <ragibmahsan@gmail.com>
2025-05-13 13:19:43 -0400

    sec

--- src/amplifyconfiguration.js
+++ src/amplifyconfiguration.js
@@ -4,7 +4,7 @@ const amplifyConfig = {
             region: 'us-east-1', // replace with your region
             userPoolId: 'us-east-1_dltWEy7Li', // replace with your User Pool ID
             userPoolClientId: '50depkvskmj5fk6o0ah3jq9sk2', // replace with your App Client ID
-            clientSecret: 'h4uqt7d3pdusm6eoan3kooms374npav1aiapj7krr79n2oej20b'
+            SECRET_HASH: 'h4uqt7d3pdusm6eoan3kooms374npav1aiapj7krr79n2oej20b'
         }
     },
 };
