221851804d43ef28850ed303bb4c0e950e23aeda
Ragib Ahsan <ragibmahsan@gmail.com>
2025-05-13 10:34:20 -0400

    updated error

--- src/aws-config.js
+++ src/aws-config.js
@@ -3,7 +3,7 @@ export const awsConfig = {
     region: "us-east-1",
     credentials: {
       accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       sessionToken: process.env.REACT_APP_AWS_SESSION_TOKEN // Optional, if using temporary credentials
     },
     // Add these for debugging
