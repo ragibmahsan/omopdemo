0a43d7ea41bb4f1e01c4a8a10fe2ca30e92655e6
Ragib Ahsan <ragibmahsan@gmail.com>
2025-05-13 13:48:32 -0400

    amp

--- src/amplifyconfiguration.js
+++ src/amplifyconfiguration.js
@@ -3,8 +3,7 @@ const amplifyConfig = {
         Cognito: {
             region: 'us-east-1', // replace with your region
             userPoolId: 'us-east-1_dltWEy7Li', // replace with your User Pool ID
-            userPoolClientId: '50depkvskmj5fk6o0ah3jq9sk2', // replace with your App Client ID
-            SECRET_HASH: 'h4uqt7d3pdusm6eoan3kooms374npav1aiapj7krr79n2oej20b'
+            userPoolClientId: '7eejh0sndhb6s9dqmq5uos15l8', // replace with your App Client ID
         }
     },
 };
